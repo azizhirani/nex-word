d5d006d42c6966eae419487ceaf0210eb9e767a5
A212007 <61305321+azizhirani@users.noreply.github.com>
2023-09-18 15:22:43 +0530

    Navbar component

---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a= "Rohan";
+const b = "karan";
+const c = "kajol";
+const d = "karina";
+
+export default b;
+export {a};
+export {d};
+export {c};
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+import sample,{a,d,c} from './module1.mjs'
+
+console.log(sample);
+console.log(a);
+console.log(d);
+console.log(c);
