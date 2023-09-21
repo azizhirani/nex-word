455ddbfa5515189823c75956dbc71b4faefed070
A212007 <61305321+azizhirani@users.noreply.github.com>
2023-09-21 17:54:13 +0530

    Updated DarkMode,About Page,used multiple Hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
+// import About from './component/About';
+// import DarkMode from '.src/component/darkmode/DarkMode';
+
 function App() {
+  const [mode,setMode]= useState('light');//check darkmode enabled state 
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-  {/* <Navbar title="A212 React App" aboutText="About A212"/> */}
-  <Navbar title="A212 React App" />
-  <div className='container  my-3'>  <TextForm heading="Enter Text to Analyze Below"/></div>
+    {/* <Navbar title="A212 React App" aboutText="About A212"/> */}
 
-  </>
+    <Navbar title="A212 Word" mode={mode} toggleMode={toggleMode}/>
+    <div className='container  my-3'>
+      <TextForm heading="Enter Text to Analyze Below" mode={mode}/>
+      {/* <About /> */}
+      </div>
+  
+    </>
   );
 }
 
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,55 +1,84 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 
+const TextForm = (props) => {
+  // text ="new text " //wrong way to change state
+  // setText ("set text Here"); //correct way to change state
 
-export default function TextForm(props) {
-    
-    // text ="new text " //wrong way to change state
-    // setText ("set text Here"); //correct way to change state
+  //convert text in upper case using usestate --> store in newText   
+  const handleUpclick = () => {
+    console.log("Uppercase was Clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
 
-    //convert text in upper case using usestate --> store in newText   
-    const handleUpclick =()=>{
-        console.log("Uppercase was Clicked" +text);
-        let newText=text.toUpperCase();
-        setText(newText);
-
+  }
+  //convert text in lower case using usestate ->store in newText
+  const handleLoclick = () => {
+    console.log("Uppercase was Clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  //Define Clear Function using useState -> store in newText
+  const handleClearclick = () => {
+    let newText = "";
+    setText(newText);
+  }
+  //Define Clear Function using useState -> store in newText
+  const handleCapclick = () => {
+    var newText = text
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+    setText(newText);
+  }
+  //Define Copy Function using navigator.clipboard.writeText()
+    const copyToClipboard = () => {
+      navigator.clipboard.writeText(text)
+      .then(() => {
+        alert('Text Copied to clipboard');
+      })
     }
-    //convert text in lower case using usestate ->store in newText
-    const handleLoclick =()=>{
-        console.log("Uppercase was Clicked" +text);
-        let newText=text.toLowerCase();
-        setText(newText);
+    //Define and remove extra spaces from text:
+    const removeExtraSpaces = () => {
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
     }
-    //define onchange function when change happens
-    const handleOnchange =(event)=>{
-        setText(event.target.value);
-    }
-    const [text,setText] = useState("");    
-  return (
-    <div className='container'>
-        <h2>{props.heading}</h2>
+//define onchange function when change happens
+const handleOnchange = (event) => {
+  setText(event.target.value);
+}
+const [text, setText] = useState("Enter text Here");
+
+return (
+  <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
+    <h2>{props.heading}</h2>
+  
     <div className="mb-3">
-  {/* <label for="exampleFormControlMail" className="form-label">Email address</label> */}
-  <input type="email" className="form-control" id="exampleFormControlMail" placeholder="name@example.com"/>
-</div>
-<div className="mb-3">
-  {/* <label for="exampleFormControltext" className="form-label" >Example text</label> */}
-  <textarea className="form-control" onChange={handleOnchange} id="exampleFormControltext" rows="3" value={text} ></textarea>
-</div >
-<button className="btn btn-secondary m-1 w-10" onClick={handleUpclick} >Convert Uppercase</button>
-<button className="btn btn-success m-1 w-10" onClick={handleLoclick} >Convert Lowercase</button>
-<br />
-<div className='container'>
-    <h1> Your Text Summary: </h1>
-    {/* <p> {text}</p> */}
+      {/* <label htmlFor="exampleFormControltext" className="form-label" >Example text</label> */}
+      <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'gray':'white',color: props.mode==='dark'?'white':'black'}}   onChange={handleOnchange} id="exampleFormControltext" rows="3" value={text} ></textarea>
+    </div >
+    <button className="btn btn-secondary m-1 w-10" onClick={handleUpclick} >Convert Uppercase</button>
+    <button className="btn btn-success m-1 w-10" onClick={handleLoclick}>Convert Lowercase</button>
+    <button className="btn btn-danger m-1 w-10" onClick={handleClearclick}>Clear Text !!</button>
+    <button className="btn btn-primary m-1 w-10" onClick={handleCapclick}>Capitalize Text</button>
+    <button className="btn btn-warning m-1 w-10" onClick={copyToClipboard}>Copy Text</button>
+    <button className="btn btn-info m-1 w-10" onClick={removeExtraSpaces}>Remove Extra Spaces</button>  
+    <br />
+    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
+      <h1> Your Text Summary: </h1>
+      {/* <p> {text}</p> */}
 
-    <p className='my-2'>{text.split(" ").length} words and {text.length} characters</p>
-    <p className='my-2'>{0.008*text.split(" ").length} Minutes to Read -  {text.length} characters</p>
-    <h3>Preview</h3>
-    <p>{text}</p>
+      <p className='my-2'>{text.split(" ").length} words and {text.length} characters</p>
+      <p className='my-2'>{0.008 * text.split(" ").length} Minutes to Read -  {text.length} characters</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
 
     </div>
 
 
-</div>
-  )
-}
+  </div>
+);
+};
+export default TextForm;
+
+
